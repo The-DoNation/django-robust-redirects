551392c87df13b758a2b20bdd6b0876fb37fe2de
Martin Warne <martmatwarne@gmail.com>
2015-05-30 14:04:58 +0100

    Updated commit_on_success to support django 1.8

--- robustredirects/management/commands/convert_redirects.py
+++ robustredirects/management/commands/convert_redirects.py
@@ -1,23 +1,31 @@
 from django.core.exceptions import ObjectDoesNotExist
-from django.core.management.base import BaseCommand, CommandError
-from robustredirects.models import Redirect
+from django.core.management.base import BaseCommand
 from django.contrib.redirects.models import Redirect as DjangoRedirect
 from django.db import transaction
+
+from robustredirects.models import Redirect
 
 
 class Command(BaseCommand):
     can_import_settings = True
 
     def handle(self, *args, **options):
-        with transaction.commit_on_success():
+        try:
+            atomic_decorator = transaction.atomic
+        except AttributeError:
+            atomic_decorator = transaction.commit_on_success
+
+        with atomic_decorator():
             count = 0
             for redirect in DjangoRedirect.objects.all():
                 try:
                     Redirect.objects.get(from_url=redirect.old_path)
-                    print("Redirect from {} already exists...skipping".format(redirect.old_path))
+                    print("Redirect from {} already exists...skipping".format(
+                        redirect.old_path))
                 except ObjectDoesNotExist:
-                    new_redirect = Redirect(from_url=redirect.old_path, to_url=redirect.new_path, site=redirect.site,
-                                            http_status=301)
+                    new_redirect = Redirect(
+                        from_url=redirect.old_path, to_url=redirect.new_path,
+                        site=redirect.site, http_status=301)
 
                     new_redirect.save()
                     count += 1
