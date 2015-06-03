0896752ca30a6a77ee2aecc23f181ba87aac09cc
Jose Antonio Martin <jose@thedonation.org.uk>
2015-06-03 12:04:54 +0100

    Added test line

--- robustredirects/middleware.py
+++ robustredirects/middleware.py
@@ -55,6 +55,10 @@ class RedirectMiddleware(object):
             if check_path.endswith('/'):
                 check_path = check_path[:-1]
 
+            if redirect.from_url == '/about/our-blog/how-nation-launch-it-rocket/':
+                print "from_url", from_url
+                print "check_path", check_path
+
             if from_url.lower() == check_path.lower():
                 if redirect.to_url == '':
                     return HttpResponseGone()
