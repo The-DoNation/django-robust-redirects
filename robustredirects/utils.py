a19bbf259455ec5105965962a3f0129145a02f6c
Martin Warne <martin@thedonation.org.uk>
2016-06-02 10:20:12 +0100

    Updated import for Django 1.10

--- robustredirects/utils.py
+++ robustredirects/utils.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, patterns
-from django.contrib.sites.models import get_current_site
+from django.contrib.sites.shortcuts import get_current_site
 
 from .models import Redirect
 
